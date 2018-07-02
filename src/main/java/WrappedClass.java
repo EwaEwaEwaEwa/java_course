ad2cffda8818dbee8881ae9128b8eae2bc7879e5
Ewa Lilith <ewabarbarakuczynska@gmail.com>
2018-07-02 22:57:55 +0200

    Added wrapped class

---
+++ src/main/java/WrappedClass.java
@@ -0,0 +1,21 @@
+public class WrappedClass {
+        public void unboxing() {
+            byte byte1 = 1;
+            Byte b = new Byte(byte1);
+            short short1 = 2;
+            Short s = new Short(short1);
+            int int1 = 5;
+            Integer number1 = new Integer(int1);
+            long long1 = 123;
+            Long l = new Long(long1);
+            float float1 = 1.0F;
+            Float f = new Float(float1);
+            double double1 = 1.111111;
+            Double d = new Double(double1);
+            char char1 = 'a';
+            Character ch = new Character(char1);
+            boolean boolean1 = true;
+            Boolean bool = new Boolean(boolean1);
+        }
+
+    }
